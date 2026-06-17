5a58a57a36073dbf3a173957b0edd32f8ec14f4f
Cesare Takeo Bruno Micheletto <cmichele@c6r1s14.42tokyo.jp>
2026-06-17 09:29:54 +0900

    complete memcmp.c ! memcmp only compare"s bytes! there is no overflow checking. memcmp doesnt care about it . its caller"s fault if (n == 0)          return (0);      while (index < n)      {          if (converted_s1[index] != converted_s2[index] )              return (converted_s1[index] - converted_s2[index]);          index++;      } in this kind of case ,i leaned you dont need to write if n == 0.... because index < n and index = 0 automatically implys 0 < 0.

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -7,25 +7,24 @@ int ft_memcmp(const void *s1, const void *s2, size_t n)
      unsigned char *converted_s1 = (unsigned char*)s1;
      unsigned char *converted_s2= (unsigned char*)s2;
      size_t index = 0;
-     if (n <= 0)
+     if (n == 0)
          return (0);
      while (index < n)
      {
-         if (converted_s1[index] != converted_s2[index] || index == n - 1)
+         if (converted_s1[index] != converted_s2[index] )
              return (converted_s1[index] - converted_s2[index]);
          index++;
      }
-     return (converted_s1[index] - converted_s2[index]);
+     return (0);
 }
 
 int main(void)
 {
-    const char *str1= "ie3loworld";
+    const char *str1= "helloworld";
     const char *str2 ="helloworld";
+    printf("the return value of my memcmp :%d\n", ft_memcmp(str1, str2, 50));
+    printf("the return value of real memcmp :%d\n", memcmp(str1, str2, 50)); // if i put n value minus. the complier detect it, but not on mine...
 
-    printf("the return value of my strncmp :%d\n",ft_memcmp(str1,str2,6));
-   printf("the return value of real strncmp :%d\n",memcmp(str1,str2,6));// if i put n value minus. the complier detect it, but not on mine...
-                                                                           
     return (0);
 }
 
