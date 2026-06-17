8360ec98aa47c96095dbb42f4ab5047eb204cc24
Cesare Takeo Bruno Micheletto <cmichele@c6r1s14.42tokyo.jp>
2026-06-17 12:02:43 +0900

    the real atoi only skips while ((*nptr >= 9 && *nptr <= 13) || *nptr == ' ') modified that part.

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -12,8 +12,8 @@ int ft_atoi(const char *nptr)
     {
         return (0);
     }
-    while (0 < *nptr && *nptr <= 32)
-        nptr++;
+    while ((*nptr >= 9 && *nptr <= 13) || *nptr == ' ')
+	    nptr++;
     if (*nptr == '-' || *nptr == '+')
     {
         if (*nptr == '-')
