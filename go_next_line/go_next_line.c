2976e60b5714889a3e578d0d168831faf5a77558
Cesare Takeo Bruno Micheletto <cmichele@c6r1s10.42tokyo.jp>
2026-08-10 11:06:34 +0900

    go next line

---
+++ go_next_line/go_next_line.c
@@ -0,0 +1,7 @@
+/* i need to read certain buffersize from file useing open() and  read() 
+in read(file) if you found '\n' then you need to stop there and store next first letter after in some stack. 
+	handling error is dificult part i guess
+
+
+go_next_line(fd);
+*/
